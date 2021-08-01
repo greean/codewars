b5dee84adfe92191ef58d5e4bf62ffe274b3680f
Andy Green <aj.green07@outlook.com>
2021-08-01 20:43:21 +0100

    currently using eval() - need to find another way!

--- linkedList.js
+++ linkedList.js
@@ -4,7 +4,25 @@ class Node {
         this.next = next;
     }
 }
+// let list = new Node(1, new Node(2, new Node(3)));
 
-let list = new Node(1, new Node(2, new Node(3)));
+function stringify(list) {
+    // Write your code here
+    let nextChain = 'list';
+    let dataChain = nextChain + '.data';
+    console.log(eval(dataChain));
+    nextChain = nextChain + '.next';
 
-console.log(list);
+    dataChain = nextChain + '.data';
+    console.log(eval(dataChain));
+    nextChain = nextChain + '.next';
+
+    dataChain = nextChain + '.data';
+    console.log(eval(dataChain));
+    nextChain = nextChain + '.next';
+    console.log(eval(nextChain));
+}
+stringify(new Node(1, new Node(2, new Node(3))));
+
+
+// console.log(list);
