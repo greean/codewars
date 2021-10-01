8b7e3ae1401f24d3f75be1597d4dff8b33da401a
Andy Green <aj.green07@outlook.com>
2021-10-01 14:12:27 +0100

    amend to linkedList.js

--- linkedList.js
+++ linkedList.js
@@ -7,22 +7,21 @@ class Node {
 // let list = new Node(1, new Node(2, new Node(3)));
 
 function stringify(list) {
-    // Write your code here
-    let nextChain = 'list';
-    let dataChain = nextChain + '.data';
-    console.log(eval(dataChain));
-    nextChain = nextChain + '.next';
+    return list === null ? "null" : `${list.data} -> ${stringify(list.next)}`;
+    
+    // let nextChain = 'list';
+    // let dataChain = nextChain + '.data';
+    // console.log(eval(dataChain));
 
-    dataChain = nextChain + '.data';
-    console.log(eval(dataChain));
-    nextChain = nextChain + '.next';
+    // nextChain = nextChain + '.next';
+    // dataChain = nextChain + '.data';
+    // console.log(eval(dataChain));
 
-    dataChain = nextChain + '.data';
-    console.log(eval(dataChain));
-    nextChain = nextChain + '.next';
-    console.log(eval(nextChain));
+    // nextChain = nextChain + '.next';
+    // dataChain = nextChain + '.data';
+    // console.log(eval(nextChain));
 }
 stringify(new Node(1, new Node(2, new Node(3))));
 
 
-// console.log(list);
+console.log(stringify(new Node(1, new Node(2, new Node(3)))));
