8d2fc8b7698bd62fe0bdf704b079d19d451fb2d5
Andy Green <aj.green07@outlook.com>
2021-10-03 23:51:28 +0100

    Refactored solution

--- sumOfNumbers.js
+++ sumOfNumbers.js
@@ -2,19 +2,14 @@
     Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
 */
 const addTogether = (a, b) => {
-    if (a === b) {                          // if a and b are equal don't add and instead return a or b
+    if (a === b) {                      // if a and b are equal don't add and instead return a or b
         return a;
-    } else if (b < a) {                     // if a is greater than b
-        let revSum = 0;
-        for (let i = b; i <= a; i++) {
-            revSum = revSum + i;
-        }
-        return revSum;
-    } else {                                // else when a is less than b
-        let sum = 0;
-        for (let i = a; i <= b; i++) {
+    } else {
+        let min = Math.min(a, b);       // set the min value
+        let max = Math.max(a, b);       // set the max value
+        let sum = 0;                    // find the sum of numbers
+        for (let i = min; i <= max; i++) {
             sum = sum + i;
-            // console.log(`Sum = ${sum}`);
         }
         return sum;
     }
